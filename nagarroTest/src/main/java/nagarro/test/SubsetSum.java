557a1e6dba93de521c1ac5186fedcfc5afcc1091
Vikas Gond <vikas.gond@nagarro.com>
2019-09-24 16:33:56 +0530

    subset logic modify

--- nagarroTest/src/main/java/nagarro/test/SubsetSum.java
+++ nagarroTest/src/main/java/nagarro/test/SubsetSum.java
@@ -2,24 +2,35 @@ package nagarro.test;
 
 public class SubsetSum {
 
-	public boolean hasSum(int[] intArr, int sum) {	
-		
+	public boolean hasSum(int[] intArr, int sum) {
+
 		return subsetSum(intArr, intArr.length - 1, sum);
 	}
-	
-	private boolean subsetSum(int[] intArr,int arrLen, int sum) {
-		
-		if(sum == 0)
+
+	private boolean subsetSum(int[] intArr, int arrLen, int sum) {
+
+		if (sum == 0)
 			return true;
-		
-		if(arrLen == 0 && sum > 0)
+
+		if (arrLen == 0 && (sum > 0 || sum < 0))
 			return false;
-		
-		if(intArr[arrLen ] > sum) {
-			return subsetSum(intArr, arrLen-1, sum);
+
+		if (sum > 0) {
+			if (intArr[arrLen] > sum) {
+				return subsetSum(intArr, arrLen - 1, sum);
+			}
+		}
+
+		if (sum < 0) {
+			if (intArr[arrLen] < sum) {
+				return subsetSum(intArr, arrLen - 1, sum - intArr[arrLen]);
+			}
 		}
 		
-		return subsetSum(intArr, arrLen-1, sum) ||  
-	            subsetSum(intArr, arrLen-1, sum-intArr[arrLen]); 
+		boolean including = subsetSum(intArr, arrLen - 1, sum);
+		boolean excluding = subsetSum(intArr, arrLen - 1, sum - intArr[arrLen]);
+		
+		//return subsetSum(intArr, arrLen - 1, sum) || subsetSum(intArr, arrLen - 1, sum - intArr[arrLen]);
+		return including || excluding;
 	}
 }

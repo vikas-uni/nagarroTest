3ceb2968748aae986fe54acd9e9c0d13074bfa06
vikasgond <vikasgond@vikasgond.Nagarro.local>
2019-10-21 15:28:08 +0530

    remove comments

--- nagarroTest/src/main/java/designPatternsJava/creational/builder/TestBuilder.java
+++ nagarroTest/src/main/java/designPatternsJava/creational/builder/TestBuilder.java
@@ -7,17 +7,11 @@ public class TestBuilder {
 
 		System.out.println(user1);
 
-		User user2 = new User.UserBuilder("Jack", "Reacher").age(40).phone("5655")
-				// no address
-				.build();
+		User user2 = new User.UserBuilder("Jack", "Reacher").age(40).phone("5655").build();
 
 		System.out.println(user2);
 
-		User user3 = new User.UserBuilder("Super", "Man")
-				// No age
-				// No phone
-				// no address
-				.build();
+		User user3 = new User.UserBuilder("Super", "Man").build();
 
 		System.out.println(user3);
 	}
